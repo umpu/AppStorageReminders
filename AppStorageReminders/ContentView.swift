4eb7a3011b25580afa9a089258805fef3e166ba9
umpu <carol.ledgers0h@icloud.com>
2024-02-07 21:46:34 +0500

    Напоминания, хранящиеся одним массивом в AppStorage

--- AppStorageReminders/ContentView.swift
+++ AppStorageReminders/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+enum FocusText {
+    case title
+}
+
 struct ContentView: View {
+    @AppStorage("reminders") private var reminders: [String] = ["Первый", "Второй"]
+    @FocusState private var focusField: FocusText?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .trailing) {
+            NavigationView {
+                List {
+                    ForEach(reminders.indices, id: \.self) { index in
+                        HStack {
+                            Image(systemName: "circle")
+                            TextField("", text: $reminders[index])
+                                .focused($focusField, equals: .title)
+                        }
+                    }
+                    .onDelete(perform: remove)
+                }
+                .listStyle(.plain)
+                .toolbar {
+                    EditButton()
+                }
+            }
+            Button("", systemImage: "plus") {
+                reminders.append("Новое напоминание")
+                focusField = .title
+            }
         }
-        .padding()
+    }
+    
+    func remove(at offsets: IndexSet) {
+        reminders.remove(atOffsets: offsets)
+    }
+}
+
+
+
+extension Array: RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode([Element].self, from: data)
+        else { return nil }
+        self = result
+    }
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else { return "[]" }
+        return result
     }
 }
 
