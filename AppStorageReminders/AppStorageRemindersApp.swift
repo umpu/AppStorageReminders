5651adafc1d0375b2db6fcb09eb742d13d76a9bb
umpu <carol.ledgers0h@icloud.com>
2024-02-07 20:51:09 +0500

    Initial Commit

---
+++ AppStorageReminders/AppStorageRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageRemindersApp.swift
+//  AppStorageReminders
+//
+//  Created by Andrei Gorbunov on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
